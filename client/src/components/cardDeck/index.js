75c768d30327994eda8e9413a97737956169a37b
jagg1991 <josephguajardo@ymail.com>
2021-06-03 17:54:37 -0700

    adding to reserve cards

--- client/src/components/cardDeck/index.js
+++ client/src/components/cardDeck/index.js
@@ -1,10 +1,42 @@
-import React from "react";
-import { Card, CardDeck, Button } from "react-bootstrap"
+import axios from "axios";
+import React, {useState, useContext} from "react";
+import { Card, CardDeck, Button, Form } from "react-bootstrap"
+import UserContext from "../../store/userContext";
 
 function Deck() {
+    const {user} = useContext(UserContext);
+    
+    if(!user.isLoggedIn) {
+        console.log("what is this madness??")
+    }
+    
+    const [reserve, setReserve] = useState({
+        reserve1: "",
+        reserve5: "",
+        reserve10: ""
+
+    })
+
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        axios.post("/api/land")
+    }
+
+    const handleChange = event => {
+        event.preventDefault();
+        const {name, value} = event.target;
+
+        setReserve({
+            ...reserve,
+            [name]: value
+        })
+    };
+    
     return (
         <>
-            <CardDeck>
+          <Form onSubmit={(e) => handleFormSubmit(e)}>
+            <CardDeck style={{marginBottom: "20px"}}>
                 <Card>
                     <Card.Img variant="top" src="moon.jpg" />
                     <Card.Body>
@@ -12,7 +44,9 @@ function Deck() {
                         <Card.Text>
                             Would you like to reserve 1 acre of land?!
                                     </Card.Text>
-                        <Button variant="dark">Reserve now!</Button>
+                        <Button name="reserve1" variant="dark" onChange={(e) => handleChange(e)} value={reserve.reserve1}>
+                            Reserve now!
+                            </Button>
                     </Card.Body>
                     <Card.Footer>
                         <small className="text-muted">Last updated 3 mins ago</small>
@@ -25,7 +59,9 @@ function Deck() {
                         <Card.Text>
                             Would you like to reserve 5 acers of Lunar land?
                                     </Card.Text>
-                        <Button variant="dark">Reserve now!</Button>
+                        <Button name="reserve5" variant="dark" onChange={(e) => handleChange(e)} value={reserve.reserve5}>
+                            Reserve now!
+                            </Button>
                     </Card.Body>
                     <Card.Footer>
                         <small className="text-muted">Last updated 3 mins ago</small>
@@ -38,13 +74,16 @@ function Deck() {
                         <Card.Text>
                             Would you like to reserve 10 acres of Lunar land?
                                      </Card.Text>
-                        <Button variant="dark">Reserve now!</Button>
+                        <Button name="reserve10" variant="dark" onChange={(e) => handleChange(e)} value={reserve.reserve10}> 
+                            Reserve now!
+                            </Button>
                     </Card.Body>
                     <Card.Footer>
                         <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
             </CardDeck>
+            </Form>
 
         </>
     )
